8164618faf2ea86f4a862b8d2b629f29ecc8c684
Ranjan9991 <97879461+Ranjan9991@users.noreply.github.com>
2022-03-07 19:34:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,183 +1,49 @@
-const axios = require('axios').default;
-async function getNews() {
-    const url = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=80e1ed93b4284b5c8c358c1ca2047c16"
-    let result = await axios.get(url)
-    // let daytime = result.data.articles[0].publishedAt.split("T")
-    // let day = daytime[0]
-    // let time = daytime[1] 
-    return result.data.articles
+const routes = require("express").Router();      // inport this in every routes files
+const handlers = require("../controllers");
+   routes.get("/",(req,res)=>{
+       const data = [
+           {"latest new api":"https://protected-garden-26257.herokuapp.com/news"},
+           {"test result":"https://protected-garden-26257.herokuapp.com/testresult"},
+           {"upcoming test":"https://protected-garden-26257.herokuapp.com/test"},
+           {"upcoming odi":"https://protected-garden-26257.herokuapp.com/odi"},
+           {"upcoming t20":"https://protected-garden-26257.herokuapp.com/t20"},
+           {"stadium detail":"https://protected-garden-26257.herokuapp.com/details"},
+           {"quiz api":"https://protected-garden-26257.herokuapp.com/quiz"},
+           {"overall match ":"https://protected-garden-26257.herokuapp.com/match"}
+       ]
+       res.json(data)
+   })
+routes.get("/news",async(req,res)=>{
+    const data = await handlers.getNews()
+    res.json(data)
+});
 
-}
-function getTestResult() {
-    const data = [
-        {
-            "date": "2022-01-10",
-            "time": "16:00IST",
-            "teams": ["India", "Srilanka"],
-            "Result":"India won",
-            "venue": "Kolkata"
-        },
-        {
-            "date": "2022-02-12",
-            "time": "11:00IST",
-            "teams": ["Australia", "Pakistan"],
-            "Result":"Australia won",
-            "venue": "Sydney"
-        }
-    ]
-    return data
-}
-function getUpcomingTest() {
-    const data =  [{
-        "date": "2022-04-10",
-        "time": "16:00IST",
-        "teams": ["India", "Srilanka"],
-        "venue": "Kolkata"
-    },
-    {
-        "date": "2022-04-12",
-        "time": "10:00IST",
-        "teams": ["Australia", "Pakistan"],
-        "venue": "Sydney"
-    },
-    {
-        "date": "2022-04-12",
-        "time": "10:00IST",
-        "teams": ["Australia", "India"],
-        "venue": "New Delhi"
-    },
-    {
-        "date": "2022-04-12",
-        "time": "10:00IST",
-        "teams": ["Bangladesh", "Pakistan"],
-        "venue": "Jaipur"
-    },
-    {
-        "date": "2022-04-12",
-        "time": "10:00IST",
-        "teams": ["New Zealand", "Nepal"],
-        "venue": "Mumbai"
-    }
-    ]
-    return data
-}
-function getUpcomingOdi() {
-    const data =  [{
-        "date": "2022-06-10",
-        "time": "16:00IST",
-        "teams": ["India", "Srilanka"],
-        "venue": "Rachi"
-    },
-    {
-        "date": "2022-04-12",
-        "time": "11:00IST",
-        "teams": ["India", "Pakistan"],
-        "venue": "Sydney"
-    },
-    {
-        "date": "2022-08-15",
-        "time": "09:00IST",
-        "teams": ["Australia", "India"],
-        "venue": "New Delhi"
-    }
-    
-    ]
-    return data
-}
-function getUpcomingT20() {
-    const data =
-        [{
-            "date": "2022-04-10",
-            "time": "16:00IST",
-            "teams": ["India", "Srilanka"],
-            "venue": "Kolkata"
-        },
-        {
-            "date": "2022-04-12",
-            "time": "10:00IST",
-            "teams": ["Australia", "Pakistan"],
-            "venue": "Sydney"
-        },
-        {
-            "date": "2022-04-12",
-            "time": "10:00IST",
-            "teams": ["Australia", "India"],
-            "venue": "New Delhi"
-        },
-        {
-            "date": "2022-04-12",
-            "time": "10:00IST",
-            "teams": ["Bangladesh", "Pakistan"],
-            "venue": "Jaipur"
-        },
-        {
-            "date": "2022-04-12",
-            "time": "10:00IST",
-            "teams": ["New Zealand", "Nepal"],
-            "venue": "Mumbai"
-        }
-        ]
-    return data;
-}
-function getStadiumDetails() {
-    const data = [{
-        "Capacity": "30000",
-        "Tenant": "Troy Trojans",
-        "Type": "Cricket Ground",
-        "Stadium": "Arun Jaitley Stadium",
-        "Location": "New delhi "
-    },
-    {
-        "Capacity": "33000",
-        "Tenant": "Troy Trojans",
-        "Type": "Cricket Ground",
-        "Stadium": "Wankhede Stadium",
-        "Location": "Mumbai "
-    }
-
-]
-   
-    return data;
-}
-function getQuiz() {
-    const data =
-    [{
-        "question1": "\n 1.Who won the cricket world cup 2019? \n A. India \n B. England \n C. New Zealand \n D. Australia ",
-        "answer": "B"
-    },
-
-     
-    {
-        "question2": "2. Who is known as Jumbo? \n A. Ishant Sharma \n B. Rahul Dravid \n C. Harbhajan Singh \n D. Anil Kumble ",
-        "answer": "D"
-    },
-
-    {
-        "question": "3. Who was the Player of the series in 2019 World Cup? \n A. Kane Williamson \n B. Ben Stokes \n C. Rohit Sharma \n D. Shakib Al Hasan ",
-        "answer": "A"
-    },
-    {
-        "question": "4. Who was the Player of the series in IPL 2020? \n A. Marcus Stoinis \n B. Trent Boult \n C. Jofra Archer \n D. Kagiso Rabada?  ",
-        "answer": "C"
-    }]
-
-    
-
-
-    return data;
-}
-function getMatchDetails() {
-    const data = 9;
-    return data;
-}
-
-module.exports = {
-    getNews,
-    getTestResult,
-    getUpcomingTest,
-    getUpcomingOdi,
-    getUpcomingT20,
-    getStadiumDetails,
-    getQuiz,
-    getMatchDetails,
-};
+routes.get("/testresult",(req,res)=>{
+	const data = handlers.getTestResult()
+    res.json(data)
+});
+routes.get("/test",(req,res)=>{
+	const data = handlers.getUpcomingTest()
+    res.json(data)
+});
+routes.get("/odi",(req,res)=>{
+const data = handlers.getUpcomingOdi()
+res.json(data)
+});
+routes.get("/t20",(req,res)=>{
+    const data = handlers.getUpcomingT20()
+    res.json(data)
+});
+routes.get("/details",(req,res)=>{
+    const data = handlers.getStadiumDetails()
+	res.json(data)
+});
+routes.get("/quiz",(req,res)=>{
+    const data = handlers.getQuiz()
+    res.json(data)
+});
+routes.get("/match",(req,res)=>{
+	const data = handlers.getMatchDetails()
+    res.json(data)
+});
+module.exports = routes;
